91ce4cef128700bd00623bdea6db0bafbe45c39e
Steve Bering <sbering@soundphysicians.com>
2019-09-13 16:27:17 -0700

    build out of metrics and chart options

--- src/components/BulletChart.vue
+++ src/components/BulletChart.vue
@@ -1,60 +1,141 @@
 <template>
+<div>
   <svg width="100%" height="65px">
     <svg preserveAspectRatio="none" viewBox="0 0 100 100">
-      <rect class="bg" fill="#ccc" width="100%" height="40"></rect>
-      <rect
-        class="data"
-        fill="#0074d9"
-        v-show="siteLevelPercentage"
-        :width="siteLevelPercentage"
-        height="40"
-      ></rect>
-      <rect
-        class="data"
-        fill="#000"
-        :width="personalLevelPercentage"
-        v-show="siteLevelPercentage"
-        y="16"
-        height="8"
-      ></rect>
+      <rect class="bg" width="100%" :height="barHeight"></rect>
+      <rect 
+        class="site-level" 
+        v-if="siteLevel" 
+        :width="siteLevelPercentage" 
+        :height="barHeight">
+        <title>{{siteLevel && siteLevel.toFixed(2)}}</title>
+      </rect>
+      <rect 
+        class="personal-level" 
+        v-if="personalLevel"
+        :width="personalLevelPercentage" 
+        height="16" 
+        y="12">
+        <title>{{personalLevel && personalLevel.toFixed(2)}}</title>
+      </rect>
+      <line 
+        class="sound-level" 
+        v-if="soundLevel" 
+        :x1="soundLevelPercentage" 
+        y1="5" 
+        :x2="soundLevelPercentage" 
+        y2="35">
+        <title>{{soundLevel && soundLevel.toFixed(2)}}</title>
+      </line>
+      <!-- <rect 
+        class="sound-level" 
+        v-if="soundLevel" 
+        :x="soundLevelPercentage"
+        width="1px" 
+        :height="barHeight"
+      >
+        <title>{{soundLevel}}</title>
+      </rect> -->
       <g class="markers">
-        <line x1="0.25" y1="0" x2="0.25" y2="50"></line>
-        <line x1="25%" y1="0" x2="25%" y2="50"></line>
-        <line x1="50%" y1="0" x2="50%" y2="50"></line>
-        <line x1="75%" y1="0" x2="75%" y2="50"></line>
-        <line x1="99.75" y1="0" x2="99.75" y2="50"></line>
+        <line v-for="increment in increments" 
+          :key="increment.position" 
+          :x1="increment.position" 
+          :x2="increment.position" 
+          y1="45" 
+          y2="60">
+        </line>
       </g>
     </svg>
     <g text-anchor="middle">
-      <text text-anchor="start" fill="#0074d9" x="0" y="60">0%</text>
-      <text fill="#0074d9" x="25%" y="60">25%</text>
-      <text fill="#0074d9" x="50%" y="60">50%</text>
-      <text fill="#0074d9" x="75%" y="60">75%</text>
-      <text text-anchor="end" fill="#0074d9" x="100%" y="60">100%</text>
+      <text 
+        v-for="increment in increments" 
+        :key="increment.position" 
+        :text-anchor="increment.anchor" 
+        :x="formatAsPercentage(increment.position)"
+        font-size=10
+        y="50"
+        class="label">
+        {{ increment.label }}
+      </text>
     </g>
   </svg>
+</div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 export default Vue.extend({
   props: {
-    dataSet: {
-      required: true
+    maxLevel: {
+      type: Number
+    },
+    partitions: {
+      type: Number,
+      default: 5
+    },
+    siteLevel: {
+      type: Number
+    },
+    personalLevel: {
+      type: Number
+    },
+    soundLevel: {
+      type: Number
     }
   },
   data() {
     return {
-      siteLevel: 45,
-      personalLevel: 38
+      barHeight: 40
     };
   },
   computed: {
-    siteLevelPercentage(): string {
-      return `${this.siteLevel.toString()}%`;
+    // if the maxLevel is an explicit number, use that as our max level. Otherwise, calculate it based on the maximum value provided to the chart
+    actualMaxLevel(): number {
+      const maxValue = Math.max(this.siteLevel || 0, this.personalLevel || 0, this.soundLevel || 0);
+      if (this.maxLevel && typeof this.maxLevel === "number") {
+        return Math.max(this.maxLevel, Math.ceil(maxValue));
+      }
+      return Math.ceil(maxValue);
     },
+
+    siteLevelPercentage(): string {
+      let value = (this.siteLevel/this.actualMaxLevel) * 100;
+      return `${value.toString()}%`;
+    },
+    
     personalLevelPercentage(): string {
-      return `${this.personalLevel.toString()}%`;
+      let value = (this.personalLevel/this.actualMaxLevel) * 100;
+      return `${value.toString()}%`;
+    },
+    
+    soundLevelPercentage(): string {
+      let value = (this.soundLevel/this.actualMaxLevel) * 100;
+      return `${value.toString()}%`;
+    },
+    
+    increments(): Array<{ position: number, label: string, anchor?: string }> {
+      let values: Array<{position: number, label: string, anchor?: string}> = [];
+      values.push({ position: 0.25, label: "0", anchor: "start" });
+      
+      // use the number of partitions provided, unless the partitions is larger than our largest number
+      // we don't want to end up with fractions in the labels
+      let actualPartitions = Math.min(this.partitions, Math.round(this.actualMaxLevel));
+
+      for (let i = 1; i < actualPartitions; i++) {
+        let position = (100 / actualPartitions) * i;
+        let label = (this.actualMaxLevel / actualPartitions) * i;
+        values.push({position: position, label: Math.round(label).toString() });
+      }
+
+      // insert the last position statically at 0.25 from the end
+      values.push( { position: 99.75, label: this.actualMaxLevel.toString(), anchor: "end" });
+
+      return values;
+    }
+  },
+  methods: {
+    formatAsPercentage(value: number) : string {
+      return `${value.toString()}%`;
     }
   }
 });
@@ -62,12 +143,33 @@ export default Vue.extend({
 
 <style lang="scss" scoped>
 svg {
-  min-width: 320px;
+  min-width: 100px;
 }
 
-line {
-  stroke: #666;
+.markers > line {
+  stroke: #888;
   stroke-width: 0.5;
-  /*   vector-effect: non-scaling-stroke; */
+}
+
+.bg {
+  fill: #D2D2D2;
+}
+
+.sound-level {
+  stroke: #000;
+  stroke-width: 0.5;
+}
+
+.site-level {
+  fill: #FAA424;
+}
+
+.personal-level {
+  fill: #2E75B6
+}
+
+.label {
+  fill: #000;
+  font-size: 10;
 }
 </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,43 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
-    <bullet-chart></bullet-chart>
-    <div style="max-width: 100px">
-      <bullet-chart></bullet-chart>
+    <div style="max-width: 600px">
+      <div class="wrapper">
+        <label><strong>Readmission Rate (7-Day (adj))</strong></label>
+        <bullet-chart
+          :max-level="100"
+          :sound-level="3.994792"
+          :site-level="33.33"
+        ></bullet-chart>
+      </div>
+      <div class="wrapper">
+        <bullet-chart
+          :max-level="100"
+          :sound-level="93.492842"
+          :site-level="94.230771"
+          :personal-level="95.833331"
+        ></bullet-chart>
+      </div>
+      <div class="wrapper">
+        <bullet-chart
+          :sound-level="4.2623858"
+          :site-level="3.375"
+          :personal-level="4"
+        ></bullet-chart>
+      </div>
+      <div class="wrapper">
+        <bullet-chart
+          :sound-level="2.1148703"
+          :site-level="1.9133333"
+          :personal-level="1.4878948"
+        ></bullet-chart>
+      </div>
+      <div class="wrapper">
+        <bullet-chart
+          :sound-level="2.1415279"
+          :site-level="1.6222892"
+          :personal-level="1.3405882"
+        ></bullet-chart>
+      </div>
     </div>
   </div>
 </template>
@@ -14,7 +48,22 @@ import BulletChart from "@/components/BulletChart.vue";
 
 export default Vue.extend({
   components: {
-    BulletChart
-  }
+    BulletChart,
+  },
 });
 </script>
+
+<style lang="scss" scoped>
+.about {
+  text-align: left;
+}
+.wrapper div:first-of-type {
+  margin-top: 10px;
+  margin-left: 20px;
+  margin-right: 20px;
+}
+.wrapper label {
+  text-align: left;
+  font-size: 8pt;
+}
+</style>
